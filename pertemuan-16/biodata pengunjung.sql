0ad3a4df4004365bfc942ff93ed0e066630ebfbb
delva <2511530021@mahasiswa.atmaluhur.ac.id>
2026-01-13 21:02:51 +0700

    prg

--- pertemuan-16/biodata pengunjung.sql
+++ pertemuan-16/biodata pengunjung.sql
@@ -5,3 +5,22 @@ create table biodata pengunjung (
     Pesan TEXT,
     Tanggal_kunjungan timestamp DEFAULT CURRENT_TIMESTAMP
 );
+
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta cahrset="utf-8">
+    <title>form biodata pengunjung</title>
+</head>
+<body>
+    <h2>form biodata pengunjung</h2>
+
+    <!-- menampilkan pesan feedback jika ada (setelah PRG) -->
+    <?PHP IF (isset($_GET['pesan'])): ?>
+        <?php if ($_get["pesan"]) == "sukses"
+            <p style="color:green;">< Data Berhasil Dikirim! Terima Kasih.</p>
+         <?php elseif ($_get["pesan"]) == "gagal"
+            <p style="color:red;">< Data Terjadi Kesalahan, silahkan coba lagi.</p>
+        <?php endif; ?>
+
+
