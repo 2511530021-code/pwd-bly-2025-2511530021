ba701949b212e2a9b5675e5e65b045efe0ec6feb
delva <2511530021@mahasiswa.atmaluhur.ac.id>
2026-01-13 20:47:52 +0700

    struktur mysql

--- pertemuan-16/biodata pengunjung.sql
+++ pertemuan-16/biodata pengunjung.sql
@@ -1,7 +1,7 @@
-create table biodata_pengunjung (
-    id INT NOT NULL AUTO_INCREMENT,
+create table biodata pengunjung (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     Nama VARCHAR(100) NOT NULL,
-    Email varchar(100) not null,
-    Pesan TEXT NOT NULL
+    Email VARCHAR(100) NOT NULL,
+    Pesan TEXT,
+    Tanggal_kunjungan timestamp DEFAULT CURRENT_TIMESTAMP
 );
-
